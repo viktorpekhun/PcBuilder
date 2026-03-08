f0470216ed88eb868c14028fcbe7f7d06286b1b5
viktorpekhun <129255792+viktorpekhun@users.noreply.github.com>
2026-03-08 19:20:12 +0200

    Add ts config

--- pcbuilderfrontend/src/main.tsx
+++ pcbuilderfrontend/src/main.tsx
@@ -5,7 +5,7 @@ import App from './App.jsx'
 import {BrowserRouter, Routes, Route} from "react-router-dom";
 import {AuthProvider} from "./context/AuthProvider.jsx";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById('root')!).render(
 
       <BrowserRouter>
           <AuthProvider>
